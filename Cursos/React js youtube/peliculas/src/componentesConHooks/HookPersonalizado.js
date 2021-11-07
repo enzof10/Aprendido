bffcf1953937736aea839d85fdcc7f3a0a318fde
enzof10 <enzo_17_05@hotmail.com>
2021-11-07 09:49:49 -0300

    hooks personalizados

--- Cursos/React js youtube/peliculas/src/componentesConHooks/HookPersonalizado.js
+++ Cursos/React js youtube/peliculas/src/componentesConHooks/HookPersonalizado.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import {useFetch} from "..//hooks/useFetch"
 // solo las peticiones que hagamos como ejemplos
 export default function HooksPersonalizados(){
+    console.log(useFetch())
     return(
         <>
             <h2>Hooks- Hooks Personalizados</h2>
--- Cursos/React js youtube/peliculas/src/hooks/useFetch.js
+++ Cursos/React js youtube/peliculas/src/hooks/useFetch.js
@@ -6,10 +6,40 @@ export const useFetch = (url)=>{
     // seria bueno que cada vez que hagamos una peticion
     // tengamos una variable que controle cuando ya se ha recibido esa peticion
     // y cuando ya se regres la respuesta se resolvio
-    const [data, setdata] = useState(null);
+    const [data, setData] = useState(null);
     const [isPending, setisPending] = useState(true);
-    const [eror, seteror] = useState(null);
-    
+    // puede traer error para en caso que haya error poderlo manipular
+    const [error, setError] = useState(null);
+
+    // se va aejecutar cuando cambie la url en el archivo donde se manda a llmaar nuestro hook personalizado
+    // es decir cuando cambie la url
+    useEffect(()=>{
+        const getData= async(url)=>{
+            try {
+            let res = await fetch(url);
+            if(!res.ok){
+                // throw era como el return de los errores
+                // es lo que va a caputar catch
+            throw {
+                    errorTry:true,
+                     status:res.status,
+                      statusText: !res.statusText ? "Ocurrio un error": res.statusText,
+                    };
+            }
+         let  data = await res.json();
+
+         setisPending(false);
+         setData(data);
+         setError({error:false})
 
 
+        } catch (errorTry) {
+            setisPending(true);
+            setError({errorTry})
+        }
+        }
+        getData(url);
+    },[url])
+// tiene que retornar algo
+    return{data, isPending, error};
 }
