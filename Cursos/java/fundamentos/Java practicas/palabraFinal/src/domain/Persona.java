2e80f1f9b3236598d932f1a9a18a45260c208f53
enzof10 <enzo_17_05@hotmail.com>
2022-01-02 18:41:09 -0300

    Matrices java

--- Cursos/java/fundamentos/Java practicas/palabraFinal/src/domain/Persona.java
+++ Cursos/java/fundamentos/Java practicas/palabraFinal/src/domain/Persona.java
@@ -1,7 +1,29 @@
 package domain;
 
 public class Persona {
+    public static final int MI_CONSTANTE= 1;
+
+    private String nombre;
+
+
     public void imprimir(){
         System.out.println("Metodo imprimir");
     }
+
+
+    public static int getMiConstante() {
+        return MI_CONSTANTE;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
 }
